40be91bf968398f02b3b43c415d7e37383e16d37
Anatolii <djtolmar@gmail.com>
2018-10-14 17:37:46 +0300

    Update check_brackets.function.php

--- check_brackets.function.php
+++ check_brackets.function.php
@@ -1,26 +1,26 @@
 <?php
 function checkBrackets($data)
 {
-	$data = preg_replace('/[^\(\)\[\]\{\}]/', '', $data); //deldete symbols but not ()[]{}
+	$data = preg_replace('/[^\(\)\[\]\{\}]/', '', $data); //remove characters except (){}[]
 	
-		$count_couple = strlen($data) / 2; // count couple
-		
-		if ( strlen($data) % 2 === 0 ? true : false) // parity check
-		{
-			// clean all couple
-			for ($i=0;$i <= $count_couple;$i++)
-			{
-				$data = str_replace( ['()', '[]', '{}'], '', $data);
-			}
-			
-			//check if !empty -> bad
-			if (!empty($data))
-			{
+	$count_couple = strlen($data) / 2; // count couple
 
-				return false;
-			} 
-			else return true;
+	if ( strlen($data) % 2 === 0 ? true : false) // parity check
+	{
+		// clean all couple
+		for ($i=0;$i <= $count_couple;$i++)
+		{
+			$data = str_replace( ['()', '[]', '{}'], '', $data);
 		}
-		else return false;
+
+		//check if !empty -> bad
+		if (!empty($data))
+		{
+
+			return false;
+		} 
+		else return true;
+	}
+	else return false;
 }
 ?>
